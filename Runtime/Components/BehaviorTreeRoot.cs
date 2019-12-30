ebde583235f90375a8589f43140fc1b16ae99087
quabug <quabug@gmail.com>
2019-12-31 02:26:58 +0800

    serialization and its sample.

---
+++ Runtime/Components/BehaviorTreeRoot.cs
@@ -0,0 +1,28 @@
+using EntitiesBT.Core;
+using EntitiesBT.Entities;
+using Unity.Entities;
+using UnityEngine;
+
+namespace EntitiesBT.Components
+{
+    [DisallowMultipleComponent]
+    public class BehaviorTreeRoot : MonoBehaviour, IConvertGameObjectToEntity
+    {
+        [SerializeField] private BTNode RootNode;
+        private void Reset()
+        {
+            RootNode = GetComponentInChildren<BTNode>();
+        }
+
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            var blobRef = new NodeBlobRef(RootNode.ToBlob());
+            var blackboard = new EntityBlackboard(dstManager, entity);
+            
+            VirtualMachine.Reset(blobRef, blackboard);
+            
+            dstManager.AddComponentData(entity, blobRef);
+            dstManager.AddComponentData(entity, new BlackboardComponent {Value = blackboard});
+        }
+    }
+}
---
+++ Runtime/Components/Extensions.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Linq;
+using EntitiesBT.Entities;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+namespace EntitiesBT.Components
+{
+    public static class BehaviorTreeExtensions
+    {
+        public static BlobAssetReference<NodeBlob> ToBlob(this BTNode root)
+        {
+            using (var builder = root.ToBlobBuilder())
+                return builder.CreateBlobAssetReference<NodeBlob>(Allocator.Persistent);
+        }
+        
+        public static unsafe BlobBuilder ToBlobBuilder(this BTNode root)
+        {
+            var nodes = root.DescendantsWithSelf().ToArray();
+            var blobSize = nodes.Select(n => n.Size).Sum();
+            var size = NodeBlob.CalculateSize(nodes.Length, blobSize);
+            var blobBuilder = new BlobBuilder(Allocator.Temp, size);
+            
+            ref var blob = ref blobBuilder.ConstructRoot<NodeBlob>();
+            var types = blobBuilder.Allocate(ref blob.Types, nodes.Length);
+            var endIndices = blobBuilder.Allocate(ref blob.EndIndices, nodes.Length);
+            var offsets = blobBuilder.Allocate(ref blob.Offsets, nodes.Length);
+            var unsafePtr = (byte*) blobBuilder.Allocate(ref blob.DataBlob, blobSize).GetUnsafePtr();
+
+            var offset = 0;
+            for (var i = 0; i < nodes.Length; i++)
+            {
+                var node = nodes[i];
+                types[i] = node.NodeId;
+                node.Index = i;
+                offsets[i] = offset;
+                node.Build(unsafePtr + offset);
+                offset += node.Size;
+
+                var endIndex = i + 1;
+                while (true)
+                {
+                    endIndices[node.Index] = endIndex;
+                    var parent = node.transform.parent;
+                    if (parent == null) break;
+                    node = parent.GetComponent<BTNode>();
+                    if (node == null) break;
+                }
+            }
+            return blobBuilder;
+        }
+        
+        public static BlobAssetReference<NodeBlob> ToBlob(this TextAsset file, int version = 0)
+        {
+            var result = BlobAssetReference<NodeBlob>.TryRead(file.bytes, version, out var blobRef);
+            if (!result) throw new FormatException($"Invalid file {file.name}");
+            return blobRef;
+        }
+    }
+}
