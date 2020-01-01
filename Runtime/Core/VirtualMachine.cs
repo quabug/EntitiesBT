25a00b2f2714c33a708d1ab3023eec82d7e6aa00
quabug <quabug@gmail.com>
2020-01-01 20:42:04 +0800

    move initialize code of VirtualMachine into its static constructor.

--- Runtime/Core/VirtualMachine.cs
+++ Runtime/Core/VirtualMachine.cs
@@ -21,35 +21,35 @@ namespace EntitiesBT.Core
             {
                 var attribute = type.GetCustomAttribute<BehaviorNodeAttribute>();
                 if (attribute == null) continue;
-                var resetFunc = type.GetMethod(attribute.ResetFunc).GetResetFunc();
-                var tickFunc = type.GetMethod(attribute.TickFunc).GetTickFunc();
+                var resetFunc = GetResetFunc(type.GetMethod(attribute.ResetFunc));
+                var tickFunc = GetTickFunc(type.GetMethod(attribute.TickFunc));
                 Register(attribute.Id, resetFunc, tickFunc);
             }
-        }
 
-        static ResetFunc GetResetFunc(this MethodInfo methodInfo)
-        {
-            return methodInfo == null
-                ? (index, blob, bb) => {}
-                : (ResetFunc)methodInfo.CreateDelegate(typeof(ResetFunc))
-            ;
-        }
+            ResetFunc GetResetFunc(MethodInfo methodInfo)
+            {
+                return methodInfo == null
+                    ? (index, blob, bb) => {}
+                    : (ResetFunc)methodInfo.CreateDelegate(typeof(ResetFunc))
+                ;
+            }
 
-        static TickFunc GetTickFunc(this MethodInfo methodInfo)
-        {
-            return methodInfo == null
-                ? (index, blob, bb) => NodeState.Running
-                : (TickFunc)methodInfo.CreateDelegate(typeof(TickFunc))
-            ;
-        }
+            TickFunc GetTickFunc(MethodInfo methodInfo)
+            {
+                return methodInfo == null
+                    ? (index, blob, bb) => NodeState.Running
+                    : (TickFunc)methodInfo.CreateDelegate(typeof(TickFunc))
+                ;
+            }
 
-        static void Register(int id, ResetFunc reset, TickFunc tick)
-        {
-            if (_resets.ContainsKey(id)) throw new DuplicateNameException($"Reset function {id} already registered");
-            if (_ticks.ContainsKey(id)) throw new DuplicateNameException($"Tick function {id} already registered");
+            void Register(int id, ResetFunc reset, TickFunc tick)
+            {
+                if (_resets.ContainsKey(id)) throw new DuplicateNameException($"Reset function {id} already registered");
+                if (_ticks.ContainsKey(id)) throw new DuplicateNameException($"Tick function {id} already registered");
 
-            _resets[id] = reset;
-            _ticks[id] = tick;
+                _resets[id] = reset;
+                _ticks[id] = tick;
+            }
         }
         
         public static NodeState Tick(INodeBlob blob, IBlackboard bb)
@@ -61,7 +61,6 @@ namespace EntitiesBT.Core
         {
             var typeId = blob.GetTypeId(index);
             var state = _ticks[typeId](index, blob, bb);
-            // Debug.Log($"[BT] tick: {index}-{node.GetType().Name}-{state}");
             return state;
         }
 
@@ -69,7 +68,6 @@ namespace EntitiesBT.Core
         {
             var typeId = blob.GetTypeId(index);
             _resets[typeId](index, blob, bb);
-            // Debug.Log($"[BT] tick: {index}-{node.GetType().Name}-{state}");
         }
 
         public static void Reset(int fromIndex, int count, INodeBlob blob, IBlackboard bb)
