e93e39ca6f24a8a0f0791610cca6ec236bcde2e5
quabug <quabug@gmail.com>
2020-01-05 23:58:16 +0800

    rotate sample

---
+++ Samples~/Entity/EntityRotate.cs
@@ -0,0 +1,51 @@
+using EntitiesBT.Core;
+using EntitiesBT.Components;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using UnityEngine;
+
+namespace EntitiesBT.Sample
+{
+    public class EntityRotate : BTNode<EntityRotateNode, EntityRotateNode.Data>
+    {
+        public Vector3 Axis;
+        public float RadianPerSecond;
+
+        public override unsafe void Build(void* dataPtr)
+        {
+            var ptr = (EntityRotateNode.Data*) dataPtr;
+            ptr->Axis = Axis;
+            ptr->RadianPerSecond = RadianPerSecond;
+        }
+    }
+    
+    [BehaviorNode("8E25032D-C06F-4AA9-B401-1AD31AF43A2F")]
+    public class EntityRotateNode
+    {
+        // optional, used for job system
+        public static ComponentType[] Types => new []
+        {
+            ComponentType.ReadWrite<Rotation>()
+          , ComponentType.ReadOnly<TickDeltaTime>()
+        };
+        
+        public struct Data : INodeData
+        {
+            public float3 Axis;
+            public float RadianPerSecond;
+        }
+
+        public static NodeState Tick(int index, INodeBlob blob, IBlackboard blackboard)
+        {
+            ref var data = ref blob.GetNodeData<Data>(index);
+            ref var rotation = ref blackboard.GetDataRef<Rotation>();
+            var deltaTime = blackboard.GetData<TickDeltaTime>();
+            rotation.Value = math.mul(
+                math.normalize(rotation.Value)
+              , quaternion.AxisAngle(data.Axis, data.RadianPerSecond * (float)deltaTime.Value.TotalSeconds)
+            );
+            return NodeState.Running;
+        }
+    }
+}
