4c4cf270cd7da92c0bca4ef5e693e04e0c2b5c13
quabug <quabug@gmail.com>
2020-02-24 00:12:51 +0800

    fix bug: repeater will reset itself every tick.

---
+++ Runtime/Nodes/RepeatDurationNode.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Linq;
+using EntitiesBT.Core;
+using EntitiesBT.Entities;
+using Unity.Entities;
+
+namespace EntitiesBT.Nodes
+{
+    [Serializable]
+    [BehaviorNode("1A5C1ED4-E5C9-402B-8862-A6106F440CCE", BehaviorNodeType.Decorate)]
+    public struct RepeatDurationNode : INodeData
+    {
+        public float CountdownSeconds;
+        public NodeState BreakStates;
+        
+        public static readonly ComponentType[] Types = { ComponentType.ReadOnly<BehaviorTreeTickDeltaTime>() };
+        
+        public static NodeState Tick(int index, INodeBlob blob, IBlackboard bb)
+        {
+            var childState = blob.TickChildren(index, bb).FirstOrDefault();
+            if (childState == 0)
+            {
+                blob.ResetChildren(index, bb);
+                childState = blob.TickChildren(index, bb).FirstOrDefault();
+            }
+            ref var data = ref blob.GetNodeData<RepeatDurationNode>(index);
+            if (data.BreakStates.HasFlag(childState)) return childState;
+
+            data.CountdownSeconds -= bb.GetData<BehaviorTreeTickDeltaTime>().Value;
+            return data.CountdownSeconds <= 0 ? NodeState.Success : NodeState.Running;
+        }
+    }
+}
