c8e5c4040fd97804eb5deed12d13893eae1f7e9f
quabug <quabug@gmail.com>
2020-02-26 21:25:14 +0800

    revert delay time node.

--- Runtime/Components/BTDelayTimer.cs
+++ Runtime/Components/BTDelayTimer.cs
@@ -1,18 +1,14 @@
 using EntitiesBT.Core;
-using EntitiesBT.Entities;
 using EntitiesBT.Nodes;
 
 namespace EntitiesBT.Components
 {
     public class BTDelayTimer : BTNode<DelayTimerNode>
     {
-        public Variable<float> DelayInSeconds;
-        
-        public override int Size => DelayInSeconds.BlobSize;
-
+        public float DelayInSeconds;
         protected override void Build(ref DelayTimerNode data, ITreeNode<INodeDataBuilder>[] builders)
         {
-            data.TimerSeconds.FromVariableUnsafe(DelayInSeconds);
+            data.TimerSeconds = DelayInSeconds;
         }
     }
 }
--- Runtime/Debug/SimpleDebugViews.cs
+++ Runtime/Debug/SimpleDebugViews.cs
@@ -4,6 +4,7 @@ namespace EntitiesBT.DebugView
 {
     // TODO: automatically generate those class by Cecil?
     public class BTDebugTimer : BTDebugView<TimerNode> {}
+    public class BTDebugDelayTimer : BTDebugView<DelayTimerNode> {}
     public class BTDebugRepeatForever : BTDebugView<RepeatForeverNode> {}
     public class BTDebugRepeatTimes : BTDebugView<RepeatTimesNode> {}
     public class BTDebugRepeatDuration : BTDebugView<RepeatDurationNode> {}
