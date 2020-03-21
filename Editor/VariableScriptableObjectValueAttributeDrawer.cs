ba1ee4d0dbc3865303212e8fa6afbdebc1182804
quabug <quabug@gmail.com>
2020-03-21 16:59:18 +0800

    able to select property in script object as variable.

--- Editor/VariableScriptableObjectValueAttributeDrawer.cs
+++ Editor/VariableScriptableObjectValueAttributeDrawer.cs
@@ -28,9 +28,14 @@ namespace EntitiesBT.Editor
                     _options = scriptableObject == null
                         ? new string[0]
                         : scriptableObject.GetType()
-                            .GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                            .GetFields(BindingFlags.Instance | BindingFlags.Public)
                             .Where(fi => fi.FieldType == _genericType)
                             .Select(fi => fi.Name)
+                            .Concat(scriptableObject.GetType()
+                                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                                .Where(pi => pi.PropertyType == _genericType && pi.CanRead)
+                                .Select(pi => pi.Name)
+                            )
                             .ToArray()
                     ;
                 }
