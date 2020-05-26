e54781c4cfd4cbd99793b59a9ec3abe7a3d67d05
quabug <quabug@gmail.com>
2020-05-26 11:45:10 +0800

    avoid "System.Reflection.ReflectionTypeLoadException" on validating behavior node while `GetTypes` from assemblies.

--- Editor/BehaviorNodeValidator.cs
+++ Editor/BehaviorNodeValidator.cs
@@ -14,7 +14,7 @@ namespace EntitiesBT.Editor
         public static void OnReload()
         {
             var dictionary = new Dictionary<int, (Type type, BehaviorNodeAttribute attribute)>(128);
-            foreach (var type in AppDomain.CurrentDomain.GetAssemblies().SelectMany(assembly => assembly.GetTypes()))
+            foreach (var type in AppDomain.CurrentDomain.GetAssemblies().SelectMany(GetTypes))
             {
                 var attributes = type.GetCustomAttributes(typeof(BehaviorNodeAttribute));
                 if (!attributes.Any()) continue;
@@ -23,6 +23,19 @@ namespace EntitiesBT.Editor
                 if (dictionary.TryGetValue(behaviorNodeAttribute.Id, out var other))
                     throw new Exception($"{other.type.FullName} has same id {behaviorNodeAttribute.Id} with {type.FullName}");
                 dictionary.Add(behaviorNodeAttribute.Id, (type, behaviorNodeAttribute));
+            }
+
+            Type[] GetTypes(Assembly assembly)
+            {
+                try
+                {
+                    return assembly.GetTypes();
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogWarning($"Cannot get types from {assembly.FullName}: {ex.Message}\n{ex.StackTrace}");
+                    return Array.Empty<Type>();
+                }
             }
         }
     }
