801d890a7e2cc453dd379871224445183400708a
quabug <quabug@gmail.com>
2020-06-08 17:13:37 +0800

    able to modify component on other Entity.

--- Runtime/Core/IEntityCommand.cs
+++ Runtime/Core/IEntityCommand.cs
@@ -22,5 +22,23 @@ namespace EntitiesBT.Core
         
         DynamicBuffer<T> AddBuffer<T>() where T : struct, IBufferElementData;
         DynamicBuffer<T> SetBuffer<T>() where T : struct, IBufferElementData;
+        
+        Entity Clone(Entity entity);
+        void Destroy(Entity entity);
+        
+        void AddComponent<T>(Entity entity, T component) where T : struct, IComponentData;
+        void AddComponent<T>(Entity entity) where T : struct, IComponentData;
+        void AddComponent(Entity entity, ComponentType componentType);
+        
+        void SetComponent<T>(Entity entity, T component) where T : struct, IComponentData;
+        
+        void RemoveComponent<T>(Entity entity);
+        void RemoveComponent(Entity entity, ComponentType componentType);
+
+        void AddSharedComponent<T>(Entity entity, T component) where T : struct, ISharedComponentData;
+        void SetSharedComponent<T>(Entity entity, T component) where T : struct, ISharedComponentData;
+        
+        DynamicBuffer<T> AddBuffer<T>(Entity entity) where T : struct, IBufferElementData;
+        DynamicBuffer<T> SetBuffer<T>(Entity entity) where T : struct, IBufferElementData;
     }
 }
--- Runtime/Entities/EntityCommandJob.cs
+++ Runtime/Entities/EntityCommandJob.cs
@@ -80,5 +80,65 @@ namespace EntitiesBT.Entities
         {
             _ecb.SetSharedComponent(_jobIndex, _entity, component);
         }
+
+        public Entity Clone(Entity entity)
+        {
+            return _ecb.Instantiate(_jobIndex, entity);
+        }
+
+        public void Destroy(Entity entity)
+        {
+            _ecb.DestroyEntity(_jobIndex, entity);
+        }
+
+        public void AddComponent<T>(Entity entity, T component) where T : struct, IComponentData
+        {
+            _ecb.AddComponent(_jobIndex, entity, component);
+        }
+
+        public void AddComponent<T>(Entity entity) where T : struct, IComponentData
+        {
+            _ecb.AddComponent<T>(_jobIndex, entity);
+        }
+
+        public void AddComponent(Entity entity, ComponentType componentType)
+        {
+            _ecb.AddComponent(_jobIndex, entity, componentType);
+        }
+        
+        public DynamicBuffer<T> AddBuffer<T>(Entity entity) where T : struct, IBufferElementData
+        {
+            return _ecb.AddBuffer<T>(_jobIndex, entity);
+        }
+        
+        public DynamicBuffer<T> SetBuffer<T>(Entity entity) where T : struct, IBufferElementData
+        {
+            return _ecb.SetBuffer<T>(_jobIndex, entity);
+        }
+        
+        public void SetComponent<T>(Entity entity, T component) where T : struct, IComponentData
+        {
+            _ecb.SetComponent(_jobIndex, entity, component);
+        }
+        
+        public void RemoveComponent<T>(Entity entity)
+        {
+            _ecb.RemoveComponent<T>(_jobIndex, entity);
+        }
+        
+        public void RemoveComponent(Entity entity, ComponentType componentType)
+        {
+            _ecb.RemoveComponent(_jobIndex, entity, componentType);
+        }
+        
+        public void AddSharedComponent<T>(Entity entity, T component) where T : struct, ISharedComponentData
+        {
+            _ecb.AddSharedComponent(_jobIndex, entity, component);
+        }
+        
+        public void SetSharedComponent<T>(Entity entity, T component) where T : struct, ISharedComponentData
+        {
+            _ecb.SetSharedComponent(_jobIndex, entity, component);
+        }
     }
 }
