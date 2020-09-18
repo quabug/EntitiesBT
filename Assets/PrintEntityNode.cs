1517b3d9b731c9e427792e4790590020be47d6d3
quabug <quabug@gmail.com>
2020-09-18 14:08:49 +0800

    test scripting graph

---
+++ Assets/PrintEntityNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Runtime;
+
+[NodeSearcherItem("PrintEntity")]
+[Serializable]
+public struct PrintEntityNode : IConstantNode
+{
+    [PortDescription(Runtime.ValueType.Entity)]
+    public InputDataPort Entity;
+
+    public void Execute<TCtx>(TCtx ctx) where TCtx : IGraphInstance
+    {
+        UnityEngine.Debug.Log(ctx.ReadEntity(Entity));
+    }
+}
