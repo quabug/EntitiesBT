fe17d5e56d5136d4ec3b76d0cc5562129064ae1d
quabug <quabug@gmail.com>
2020-10-11 16:14:03 +0800

    enable to fetch data from graph instance at runtime.

---
+++ Packages/builder.visual/Runtime/GraphInstanceComponent.cs
@@ -0,0 +1,10 @@
+using Runtime;
+using Unity.Entities;
+
+namespace EntitiesBT.Builder.Visual
+{
+    public class GraphInstanceComponent : IComponentData
+    {
+        public GraphInstance Value;
+    }
+}
