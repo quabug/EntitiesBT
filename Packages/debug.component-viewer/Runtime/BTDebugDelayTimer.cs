a93e453efb33d365d4e3281d0398d213e2eb8c02
quabug <quabug@gmail.com>
2021-01-14 14:37:18 +0800

    fix debug view of delay timer node.

---
+++ Packages/debug.component-viewer/Runtime/BTDebugDelayTimer.cs
@@ -0,0 +1,20 @@
+using EntitiesBT.Core;
+using EntitiesBT.Entities;
+using EntitiesBT.Nodes;
+
+namespace EntitiesBT.DebugView
+{
+    public class BTDebugDelayTimer : BTDebugView<DelayTimerNode>
+    {
+        public float DelayTime;
+
+        public override void Tick()
+        {
+            if (!Blackboard.HasValue) return;
+
+            var blob = Blob;
+            var bb = Blackboard.Value;
+            DelayTime = blob.GetNodeData<DelayTimerNode, NodeBlobRef>(Index).TimerSecondsReader.Read(Index, ref blob, ref bb);
+        }
+    }
+}
