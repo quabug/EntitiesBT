046bea22a6b107bd6aec53affc03f7e37b79aaf1
quabug <quabug@gmail.com>
2021-01-31 02:06:17 +0800

    note

--- Packages/essential/Dependencies/Attributes/Editor/MultiPropertyUnityDrawer.cs
+++ Packages/essential/Dependencies/Attributes/Editor/MultiPropertyUnityDrawer.cs
@@ -8,11 +8,13 @@ namespace EntitiesBT.Attributes.Editor
     {
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
+            // [optimize] TODO: only call last `GetHeight` of same `fieldInfo` in one frame
             return MultiPropertyDrawerRegister.GetHeight(fieldInfo)(property, label);
         }
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
+            // [optimize] TODO: only call last `DrawMultiProperty` of same `fieldInfo` in one frame
             MultiPropertyDrawerRegister.DrawMultiProperty(fieldInfo)(position, property, label);
         }
     }
