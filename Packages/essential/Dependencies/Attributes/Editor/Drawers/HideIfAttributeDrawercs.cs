7a8cc11edfa3579b4e6390e5d5ad0e8e5add6878
quabug <quabug@gmail.com>
2021-01-31 01:55:40 +0800

    able to leave empty space while hidding.

--- Packages/essential/Dependencies/Attributes/Editor/Drawers/HideIfAttributeDrawercs.cs
+++ Packages/essential/Dependencies/Attributes/Editor/Drawers/HideIfAttributeDrawercs.cs
@@ -6,15 +6,19 @@ namespace EntitiesBT.Attributes.Editor
     [CustomMultiPropertyDrawer(typeof(HideIfAttribute))]
     public class HideIfAttributeDrawer : BaseMultiPropertyDrawer
     {
+        private HideIfAttribute _attribute => (HideIfAttribute) Decorator;
+
         private bool ShouldHide(SerializedProperty property)
         {
-            var attribute = (HideIfAttribute) Decorator;
-            return (bool) property.GetSiblingFieldValue(attribute.ConditionFieldName) == attribute.Value;
+            return (bool) property.GetSiblingFieldValue(_attribute.ConditionFieldName) == _attribute.Value;
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
-            return ShouldHide(property) ? 0 : base.GetPropertyHeight(property, label);
+            return ShouldHide(property)
+                ? _attribute.LeaveEmptySpace ? EditorGUI.GetPropertyHeight(property, false) : 0
+                : base.GetPropertyHeight(property, label)
+            ;
         }
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
