fdcfb55686a42e769b971a5e757b2a67dfeacf00
quabug <quabug@gmail.com>
2021-01-31 00:43:27 +0800

    button of `SerializeReference`

--- Packages/essential/Dependencies/Attributes/Editor/MultiPropertyUnityDrawer.cs
+++ Packages/essential/Dependencies/Attributes/Editor/MultiPropertyUnityDrawer.cs
@@ -7,6 +7,11 @@ namespace EntitiesBT.Attributes.Editor
     [CustomPropertyDrawer(typeof(MultiPropertyAttribute), true)]
     public class MultiPropertyUnityDrawer : PropertyDrawer
     {
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return EditorGUI.GetPropertyHeight(property, true);
+        }
+
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             var drawers = MultiPropertyDrawerRegister.GetOrCreateDrawers(fieldInfo);
