3f5c07a45d180d842cc9e20e4b35c798e8795432
quabug <quabug@gmail.com>
2021-07-27 01:19:52 +0800

    fix unit test.

--- Packages/samples/Tests~/Editor/TestExpression.cs
+++ Packages/samples/Tests~/Editor/TestExpression.cs
@@ -1,7 +1,9 @@
+using System;
 using EntitiesBT.Core;
 using EntitiesBT.Entities;
 using EntitiesBT.Variant;
 using EntitiesBT.Variant.Expression;
+using Moq;
 using NUnit.Framework;
 using Unity.Collections;
 using Unity.Entities;
@@ -13,15 +15,40 @@ namespace EntitiesBT.Test
     public class TestExpression
     {
         private BlobBuilder _builder;
-        private NodeBlobRef _nodeBlob;
-        private EntityBlackboard _bb;
+        private MockNodeBlob _nodeBlob;
+        private MockBlackboard _bb;
+
+        private struct MockNodeBlob : INodeBlob
+        {
+            public int RuntimeId { get; }
+            public int Count { get; }
+            public int GetTypeId(int nodeIndex) => throw new NotImplementedException();
+            public int GetEndIndex(int nodeIndex) => throw new NotImplementedException();
+            public int GetNodeDataSize(int startNodeIndex, int count = 1) => throw new NotImplementedException();
+            public NodeState GetState(int nodeIndex) => throw new NotImplementedException();
+            public void SetState(int nodeIndex, NodeState state) => throw new NotImplementedException();
+            public void ResetStates(int index, int count = 1) => throw new NotImplementedException();
+            public IntPtr GetDefaultDataPtr(int nodeIndex) => throw new NotImplementedException();
+            public IntPtr GetRuntimeDataPtr(int nodeIndex) => throw new NotImplementedException();
+        }
+
+        private struct MockBlackboard : IBlackboard
+        {
+            public bool HasData<T>() where T : struct => throw new NotImplementedException();
+            public T GetData<T>() where T : struct => throw new NotImplementedException();
+            public ref T GetDataRef<T>() where T : struct => throw new NotImplementedException();
+            public bool HasData(Type type) => throw new NotImplementedException();
+            public IntPtr GetDataPtrRO(Type type) => throw new NotImplementedException();
+            public IntPtr GetDataPtrRW(Type type) => throw new NotImplementedException();
+            public T GetObject<T>() where T : class => throw new NotImplementedException();
+        }
 
         [SetUp]
         public void SetUp()
         {
             _builder = new BlobBuilder(Allocator.Temp);
-            _nodeBlob = new NodeBlobRef();
-            _bb = new EntityBlackboard();
+            _nodeBlob = new MockNodeBlob();
+            _bb = new MockBlackboard();
         }
 
         [TearDown]
@@ -44,7 +71,7 @@ namespace EntitiesBT.Test
             ref var buildVariant = ref _builder.ConstructRoot<BlobVariant>();
             expression.Allocate(ref _builder, ref buildVariant, null, null);
             using var variant = _builder.CreateBlobAssetReference<BlobVariant>(Allocator.Temp);
-            var result = BlobVariantExtension.Read<float, NodeBlobRef, EntityBlackboard>(ref variant.Value, 0, ref _nodeBlob, ref _bb);
+            var result = BlobVariantExtension.Read<float, MockNodeBlob, MockBlackboard>(ref variant.Value, 0, ref _nodeBlob, ref _bb);
             Assert.That(result, Is.EqualTo((4.1f + 5) * 2 + 10));
         }
     }
